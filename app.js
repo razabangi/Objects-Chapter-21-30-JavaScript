eacf951153baaca2e3b26e0bb5c10dcfe182cda0
Muhammadrazabangi09 <muhammad.raza@cooperativecomputing.com>
2021-11-13 16:28:11 +0500

    from chapter 21 to 25 question 6 complete

---
+++ app.js
@@ -0,0 +1,39 @@
+// Start of Chapter No 21 to 25
+
+// Answer no 1:
+// var firstName = prompt("Enter your first name");
+// var lastName = prompt("Enter your last name");
+// var fullName = `${firstName} ${lastName}`;
+// document.write(fullName);
+
+// Answer no 2:
+// var mobile = prompt("Enter your favourite mobile phone!");
+// var textLength = mobile.length;
+// document.write(`Your favorite phone is: ${mobile}<br>`);
+// document.write(`Length of string: ${textLength}<br>`);
+
+// Answer no 3:
+// var nationality = "Pakistani";
+// var findIndex = nationality.indexOf("n");
+// document.write(`${typeof nationality}: ${nationality}<br>`);
+// document.write(`Index of 'n': ${findIndex}`);
+
+// Answer no 4:
+// var text = "Hello world!";
+// var findLastIndex = text.lastIndexOf("l");
+// document.write(`${typeof text}: ${text}<br>`);
+// document.write(`Last index of 'l': ${findLastIndex}`);
+
+// Answer no 5:
+// var nationality = "Pakistani";
+// var findChar = nationality.charAt(3);
+// document.write(`${typeof nationality}: ${nationality}<br>`);
+// document.write(`Character at index 3: ${findChar}`);
+
+// Answer no 6:
+// var firstName = prompt("Enter your first name");
+// var lastName = prompt("Enter your last name");
+// var fullName = firstName.concat(" ",lastName);
+// document.write(fullName);
+
+// End of Chapter No 21 to 25
